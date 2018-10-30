306381c250fe74b88efb95525d56837a2c8eebe5
yuan1998 <chizhiyueshu@gmail.com>
2018-10-30 18:01:25 +0800

    examples gird fix: change layout file name to gird

--- examples/app.vue
+++ examples/app.vue
@@ -2,9 +2,15 @@
     <section class="examples-container">
         <aside class="examples-nav">
             <ul>
-                <li><router-link to="/button">Button</router-link></li>
-                <li><router-link to="/layout">Layout</router-link></li>
-                <li><router-link to="/tooltip">Tooltip</router-link></li>
+                <li>
+                    <router-link to="/button">Button</router-link>
+                </li>
+                <li>
+                    <router-link to="/layout">Gird</router-link>
+                </li>
+                <li>
+                    <router-link to="/tooltip">Tooltip</router-link>
+                </li>
             </ul>
         </aside>
         <section class="examples-content">
@@ -14,44 +20,47 @@
 </template>
 
 <style lang="less">
-.examples-container {
-    min-height: 100vh;
-    width: 100%;
-    display: flex;
-}
-.examples-nav {
-    width: 220px;
-    flex: none;
-    border-right: 1px solid #ddd;
-    padding: 12px 0;
+    .examples-container {
+        min-height: 100vh;
+        width: 100%;
+        display: flex;
+    }
 
-    >ul {
-        > li {
-            height: 34px;
-            transition: all 0.2s ease-in;
+    .examples-nav {
+        width: 220px;
+        flex: none;
+        border-right: 1px solid #ddd;
+        padding: 12px 0;
 
-            > a {
+        > ul {
+            > li {
+                height: 34px;
                 transition: all 0.2s ease-in;
-                display: block;
-                padding: 0 10px;
-                line-height: 34px;
-                font-size: 16px;
-                cursor: pointer;
 
-                &.router-link-active,
-                &:hover {
-                    background-color: #2d8cf0;
-                    color: #fff;
+                > a {
+                    transition: all 0.2s ease-in;
+                    display: block;
+                    padding: 0 10px;
+                    line-height: 34px;
+                    font-size: 16px;
+                    cursor: pointer;
+
+                    &.router-link-active,
+                    &:hover {
+                        background-color: #2d8cf0;
+                        color: #fff;
+                    }
                 }
             }
         }
     }
-}
-.examples-content {
-    padding: 20px;
-    flex: auto;
-}
-.mt {
-    margin-top: 20px;
-}
+
+    .examples-content {
+        padding: 20px;
+        flex: auto;
+    }
+
+    .mt {
+        margin-top: 20px;
+    }
 </style>
