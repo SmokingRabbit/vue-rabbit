2fb6d739edfe9752f54e527e716abd5034dc7636
yuan1998 <chizhiyueshu@gmail.com>
2018-10-30 18:30:45 +0800

    component layout update: main complete

---
+++ src/components/layout/main.tsx
@@ -0,0 +1,24 @@
+import Vue, {CreateElement, VNode} from 'vue';
+import {Component} from 'vue-property-decorator';
+import {prefixCls} from "../../utils/assist";
+
+@Component
+
+class Main extends Vue {
+
+    private get className(): object {
+        return {
+            [`${prefixCls}main`]: true
+        };
+    }
+
+    public render(h: CreateElement): VNode {
+        const {$slots, className} = this;
+
+        return (
+            <main class={className}>{$slots.default}</main>
+        );
+    }
+}
+
+export default Main;
