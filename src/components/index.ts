ba3621305374f6348d5e3472533a1d6746b029ed
yuan1998 <chizhiyueshu@gmail.com>
2018-10-30 18:47:02 +0800

    component layout update: include layout component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Button } from './button';
 export { Row, Col } from './grid';
 export { default as Popup } from './popup';
 export { default as Tooltip } from './tooltip';
+export { Layout , Header , Footer , Aside , Main } from './layout';
---
+++ src/components/layout/index.ts
@@ -0,0 +1,13 @@
+import Layout from './layout';
+import Header from './header';
+import Footer from './footer';
+import Aside from './aside';
+import Main from './main';
+
+export {
+    Layout,
+    Header,
+    Footer,
+    Main,
+    Aside
+};
