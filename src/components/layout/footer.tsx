eb35e2480a51f8954c2a513714361b974338edce
yuan1998 <chizhiyueshu@gmail.com>
2018-10-30 18:40:24 +0800

    component layout update: footer complete

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,37 @@
+import Vue, {CreateElement, VNode} from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import {prefixCls} from '../../utils/assist';
+
+@Component
+
+class Footer extends Vue {
+
+    @Prop({
+        type: String,
+        default: '60px'
+    })
+    public height!: string;
+
+    private get className (): object {
+        return {
+            [`${prefixCls}footer`] : true
+        }
+    }
+
+    private get styleName (): object {
+        const { height } = this;
+
+        return {
+            height,
+        }
+    }
+
+    public render(h: CreateElement): VNode {
+        const { $slots , className , styleName } = this;
+        return (
+            <footer style={styleName} class={className}>{$slots.default}</footer>
+        )
+    }
+}
+
+export default Footer;
